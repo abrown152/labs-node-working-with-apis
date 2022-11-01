c4f57d2d39f18f6d143a35136569a6fd352d0a6b
abrown152 <alysiacbrown@gmail.com>
2022-11-01 12:15:08 -0700

    Lab compleeted.

--- src/web-app/index.js
+++ src/web-app/index.js
@@ -17,6 +17,7 @@ const logger = require("morgan");
 const path = require("path");
 const { createServer } = require("http");
 const { auth, requiresAuth } = require("express-openid-connect");
+const axios = require("axios").default;
 
 const app = express();
 
@@ -43,44 +44,46 @@ app.use(
     authRequired: false,
     auth0Logout: true,
     baseURL: APP_URL,
+    authorizationParams: {
+      response_type: "code id_token",
+      audience: "https://expenses-api",
+    },
   })
 );
 
-const expenses = [
-  {
-    date: new Date(),
-    description: "Pizza for a Coding Dojo session.",
-    value: 102,
-  },
-  {
-    date: new Date(),
-    description: "Coffee for a Coding Dojo session.",
-    value: 42,
-  },
-];
+ // This comment will replace the hard-coded expenses data.
 
-app.get("/", async (req, res) => {
-  res.render("home", {
-    user: req.oidc && req.oidc.user,
-    total: expenses.reduce((accum, expense) => accum + expense.value, 0),
-    count: expenses.length,
-  });
-});
-
-app.get("/user", requiresAuth(), async (req, res) => {
-  res.render("user", {
-    user: req.oidc && req.oidc.user,
-    id_token: req.oidc && req.oidc.idToken,
-    access_token: req.oidc && req.oidc.accessToken,
-    refresh_token: req.oidc && req.oidc.refreshToken,
-  });
+app.get("/", async (req, res, next) => {
+  try {
+     const summary = await axios.get(`${API_URL}/total`);
+     res.render("home", {
+      user: req.oidc && req.oidc.user,
+      total: summary.data.total,
+      count: summary.data.count,
+     });
+  } catch (err) {
+     next(err);
+  }
 });
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
-  res.render("expenses", {
-    user: req.oidc && req.oidc.user,
-    expenses,
+  try {
+     // 👇 get the token from the request 👇
+     const { token_type, access_token } = req.oidc.accessToken;
+     // 👇 then send it as an authorization header 👇
+     const expenses = await axios.get(`${API_URL}/reports`, {
+       headers: {
+         Authorization: `${token_type} ${access_token}`,
+       },
+     });
+     // 👆 end of changes 👆
+     res.render("expenses", {
+      user: req.oidc && req.oidc.user,
+      expenses: expenses.data,
   });
+  } catch (err) {
+     next(err);
+  }
 });
 
 // catch 404 and forward to error handler
